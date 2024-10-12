5683d46e84213616f0772f188713360f84c36497
Hyunsoo Kang <air10151015@gmail.com>
2024-10-12 22:17:21 +0900

    고블린 초기화 ㅆ
    고블린 설정 초기화 다시 구성시작

--- Assets/Script/GoblinController.cs
+++ Assets/Script/GoblinController.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
-using System;
+using TMPro;
+using System;  // Action 이벤트 사용을 위해 추가
 
 public class GoblinController : MonoBehaviour
 {
-    public Action OnGoblinDestroyed;  // 고블린이 파괴되었을 때 호출되는 이벤트
+    public TextMeshProUGUI numberText;  // 고블린 머리 위에 표시될 숫자 텍스트
+    private int assignedNumber;  // 고블린에 할당된 랜덤 숫자
 
-    // 고블린이 파괴될 때 호출
-    void OnDestroy()
+    // 고블린이 파괴될 때 호출될 이벤트
+    public Action OnGoblinDestroyed;
+
+    void Start()
     {
-        if (OnGoblinDestroyed != null)
+        // 고블린에게 랜덤한 숫자 할당
+        assignedNumber = UnityEngine.Random.Range(1, 10);
+        UpdateNumberText();
+    }
+
+    // 고블린 머리 위에 숫자를 표시하는 함수
+    void UpdateNumberText()
+    {
+        if (numberText != null)
         {
-            OnGoblinDestroyed.Invoke();  // 파괴 이벤트 호출
+            numberText.text = assignedNumber.ToString();
         }
+    }
+
+    // 플레이어가 입력한 숫자를 확인하고 고블린을 죽이는 함수
+    public void CheckPlayerInput(int inputNumber)
+    {
+        if (inputNumber == assignedNumber)
+        {
+            Die();  // 숫자가 일치하면 고블린 사망 처리
+        }
+    }
+
+    // 고블린 사망 처리
+    void Die()
+    {
+        // 고블린 파괴 이벤트 호출
+        OnGoblinDestroyed?.Invoke();
+
+        // 고블린 파괴
+        Destroy(gameObject);
     }
 }
--- Assets/Script/GoblinSpawner.cs
+++ Assets/Script/GoblinSpawner.cs
@@ -3,57 +3,61 @@ using System.Collections;
 
 public class GoblinSpawner : MonoBehaviour
 {
-    public GameObject goblinPrefab;  // 고블린 프리팹
-    public Transform[] spawnPoints;  // 1, 2, 3층의 스폰 위치
-    public float minSpawnTime = 2f;  // 최소 스폰 시간 간격
-    public float maxSpawnTime = 5f;  // 최대 스폰 시간 간격
+    // 각 층에 배치될 고블린 프리팹
+    public GameObject goblinPrefabFloor1;
+    public GameObject goblinPrefabFloor2;
+    public GameObject goblinPrefabFloor3;
 
-    private bool[] hasGoblin;  // 각 층에 고블린이 있는지 여부를 추적
+    // 1층, 2층, 3층의 스폰 위치
+    public Transform[] spawnPoints;  // 0: 1층, 1: 2층, 2: 3층
 
-    // 스테이지가 시작되면 스포닝을 시작
+    public float spawnTime = 5f;  // 고블린 스폰 시간 간격
+
+    private bool[] hasGoblin = new bool[3];  // 각 층에 고블린이 있는지 여부 추적
+
     void Start()
     {
-        hasGoblin = new bool[spawnPoints.Length];  // 각 층의 고블린 상태 초기화
-        StartCoroutine(SpawnGoblins());
+        StartCoroutine(SpawnGoblin());
     }
 
-    // 고블린을 일정 시간 간격으로 랜덤하게 스폰
-    IEnumerator SpawnGoblins()
+    IEnumerator SpawnGoblin()
     {
-        while (true)  // 스테이지가 끝날 때까지 반복
+        while (true)
         {
-            // 랜덤한 시간 대기
-            float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
-            yield return new WaitForSeconds(waitTime);
-
             // 고블린이 없는 층을 찾음
-            int randomIndex = GetRandomEmptyFloor();
+            int randomFloor = GetRandomEmptyFloor();
 
-            // 고블린이 없는 층이 있을 경우에만 고블린을 생성
-            if (randomIndex != -1)
+            if (randomFloor != -1)  // 고블린이 없는 층이 있을 때만 스폰
             {
-                Transform spawnLocation = spawnPoints[randomIndex];
+                Transform spawnLocation = spawnPoints[randomFloor];
 
-                // 고블린 생성 (y축으로 180도 회전)
+                // 해당 층에 맞는 고블린 프리팹 선택
+                GameObject goblinPrefab = GetGoblinPrefab(randomFloor);
+
+                // 고블린 생성
                 GameObject goblin = Instantiate(goblinPrefab, spawnLocation.position, Quaternion.Euler(0, 180, 0));
 
-                // 고블린이 생성된 층 상태 업데이트
-                hasGoblin[randomIndex] = true;
+                // 해당 층에 고블린이 존재한다고 상태 변경
+                hasGoblin[randomFloor] = true;
 
-                // 고블린이 파괴될 때 호출될 이벤트 설정
-                goblin.GetComponent<GoblinController>().OnGoblinDestroyed += () => hasGoblin[randomIndex] = false;
+                // 고블린이 파괴되면 해당 층 상태를 비워둠
+                GoblinController goblinController = goblin.GetComponent<GoblinController>();
+                goblinController.OnGoblinDestroyed += () => hasGoblin[randomFloor] = false;
             }
+
+            // 스폰 대기 시간
+            yield return new WaitForSeconds(spawnTime);
         }
     }
 
-    // 고블린이 없는 랜덤한 층을 선택하는 함수
+    // 고블린이 없는 층을 랜덤하게 선택하는 함수
     int GetRandomEmptyFloor()
     {
         // 고블린이 없는 층을 찾음
-        int[] emptyFloors = new int[spawnPoints.Length];
+        int[] emptyFloors = new int[3];
         int emptyCount = 0;
 
-        for (int i = 0; i < spawnPoints.Length; i++)
+        for (int i = 0; i < hasGoblin.Length; i++)
         {
             if (!hasGoblin[i])
             {
@@ -65,7 +69,19 @@ public class GoblinSpawner : MonoBehaviour
         // 고블린이 없는 층이 없다면 -1 반환
         if (emptyCount == 0) return -1;
 
-        // 고블린이 없는 층 중에서 랜덤하게 선택
+        // 고블린이 없는 층 중 하나를 랜덤하게 선택
         return emptyFloors[Random.Range(0, emptyCount)];
+    }
+
+    // 층에 맞는 고블린 프리팹 반환
+    GameObject GetGoblinPrefab(int floorIndex)
+    {
+        switch (floorIndex)
+        {
+            case 0: return goblinPrefabFloor1;  // 1층
+            case 1: return goblinPrefabFloor2;  // 2층
+            case 2: return goblinPrefabFloor3;  // 3층
+            default: return null;
+        }
     }
 }
